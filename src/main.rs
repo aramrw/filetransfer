973e1a480b9790e99938ca57d5bfd96d50c8c5e0
aramrw <106574385+aramrw@users.noreply.github.com>
2025-03-20 21:07:33 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
-mod router;
+mod cli;
 mod dl;
+mod router;
 mod upload;
+use cli::PDATA;
+use local_ip_address::local_ip;
 use router::APP;
-use std::{fs, net::SocketAddr, path::Path};
+use std::{
+    fs,
+    net::{Ipv4Addr, SocketAddr},
+    path::Path,
+};
 
 #[tokio::main]
 async fn main() {
+    println!("{}", *PDATA);
     if !Path::new("./upload").exists() {
         fs::create_dir_all("./upload").expect("Failed to create upload directory");
     }
     //let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-    let tcpl = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let tcpl = tokio::net::TcpListener::bind(PDATA.cli.addr).await.unwrap();
     axum::serve(
         tcpl,
         APP.clone()
